c4ba9a360e963b4fa2ec87cb71bb96b25245ba0d
Paweł Olejniczak <pawel.olejniczak@gmail.com>
2014-01-06 11:16:28 +0000

    change default time format

--- gunnery/settings.py
+++ gunnery/settings.py
@@ -77,10 +77,10 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
-
+DATETIME_FORMAT = 'Y-m-d H:i'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
