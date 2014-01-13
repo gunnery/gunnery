e30dbd2d1bd56a1740ce541df16756dd3139d093
Eyjafjallajokull <pawel.olejniczak@gmail.com>
2014-01-13 20:39:11 +0100

    change db to postgres

--- gunnery/settings.py
+++ gunnery/settings.py
@@ -64,8 +64,11 @@ WSGI_APPLICATION = 'gunnery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gunnery',
+        'USER': 'gunnery',
+        'PASSWORD': 'gunnery',
+        'HOST': '127.0.0.1',
     }
 }
 
