1236c7ec73293764a9d1dfcc9515213a5873c867
Eyjafjallajokull <pawel.olejniczak@gmail.com>
2014-03-02 19:41:01 +0100

    fix production config

--- gunnery/gunnery/settings/production.py
+++ gunnery/gunnery/settings/production.py
@@ -5,3 +5,11 @@ EMAIL_HOST = 'localhost'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 25
+
+
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
