58869a3ab80dad7de2b8e4be75aa81f4058fda54
Eyjafjallajokull <pawel.olejniczak@gmail.com>
2014-03-02 22:14:06 +0100

    fix production settings

--- gunnery/gunnery/settings/production.py
+++ gunnery/gunnery/settings/production.py
@@ -13,3 +13,21 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console_error': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console_error'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
